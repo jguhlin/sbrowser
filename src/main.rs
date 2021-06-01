63657675f1d5f19130bae35ab70e582782039894
jguhlin <joseph.guhlin@gmail.com>
2021-06-02 08:54:08 +1200

    zoom

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
-use bevy::prelude::*;
+use bevy::{
+    prelude::*,
+    input::mouse::MouseWheel,
+};
+
 use bevy_egui::{egui, EguiContext, EguiPlugin};
 use bevy_inspector_egui::Inspectable;
 use bevy_prototype_lyon::prelude::*;
+use bevy_inspector_egui::InspectorPlugin;
+use bevy_inspector_egui::WorldInspectorPlugin;
 
 mod genome;
 
@@ -20,18 +26,35 @@ fn main() {
         .insert_resource(genome)
         .insert_resource(UISetting { zoom_factor: 1024.0 })
         .add_plugins(DefaultPlugins)
+        .add_plugin(WorldInspectorPlugin::new())
         .add_plugin(ShapePlugin)
         .add_startup_system(setup.system())
         .add_startup_system(draw_chromosome.system())
         .add_system(camera_move.system())
+        .add_system(mouse_scroll.system())
+        // .add_system(zoom_chromosome.system())
         .run();
 }
 
 #[derive(Default)]
 pub struct Camera;
+pub struct Chromosome;
+
+fn zoom_chromosome(mut commands: Commands, genome: Res<Genome>, 
+    mut query: Query<(&Chromosome, &mut Transform)>,
+    ui_settings: Res<UISetting>, // TODO: Should be an event
+) {
+    if !ui_settings.is_changed() {
+        return
+    }
+
+    for (_camera, mut transform) in query.iter_mut() {
+        println!("Running");
+        transform.scale += Vec3::new(0.01, 0.01, 0.0);
+    }
+}
 
 fn draw_chromosome(mut commands: Commands, genome: Res<Genome>, ui_settings: Res<UISetting>) {
-
     let zf = ui_settings.zoom_factor;
     let width = genome.length as f32 / zf; // 1024 bp per pixel
 
@@ -50,7 +73,7 @@ fn draw_chromosome(mut commands: Commands, genome: Res<Genome>, ui_settings: Res
             outline_options: StrokeOptions::default().with_line_width(10.0),
         },
         Transform::default(),
-    )); 
+    )).insert(Chromosome); 
 
     for gene in &genome.genes {
 
@@ -69,7 +92,7 @@ fn draw_chromosome(mut commands: Commands, genome: Res<Genome>, ui_settings: Res
 //        let transform = Transform::default();
 
         let coords = calc_coords(&genome, zf, gene);
-        println!("{:#?}", coords);
+        // println!("{:#?}", coords);
 
 //        let transform = Transform::from_translation(Vec3::new(start, -50.0, 1.0));
         let transform = Transform::from_translation(coords);
@@ -112,8 +135,6 @@ fn calc_coords(genome: &Genome, zf: f32, gene: &Gene) -> Vec3 {
 
     let zero = -width/2.0;
 
-    println!("{:#?}", zero);
-
     let start_loc = zero + (gene.start as f32 / zf);
     let end_loc = zero + (gene.end as f32 / zf);
 
@@ -151,4 +172,44 @@ fn camera_move(
             transform.translation += velocity * time.delta_seconds() * 100.0
         }
     }
+}
+fn mouse_scroll(
+    mut mouse_wheel_events: EventReader<MouseWheel>,
+    time: Res<Time>,
+    windows: Res<Windows>,
+    mut ui_setting: ResMut<UISetting>,
+    mut query: Query<(&Camera, &mut Transform)>,
+) {
+    let window = windows.get_primary().unwrap();
+
+    let (_camera, mut transform) = query.single_mut().unwrap();
+
+    for event in mouse_wheel_events.iter() {
+        ui_setting.zoom_factor + event.y;
+        transform.scale += -event.y * Vec3::new(0.01, 0.01, 0.0);
+    }
+
+    /*
+ 
+    for (_camera, mut transform) in query.iter_mut() {
+        let mut velocity = Vec3::ZERO;
+        let vert = Vec3::new(0.0, 1.0, 0.0);
+        let horiz = Vec3::new(1.0, 0.0, 0.0);
+
+        for key in keys.get_pressed() {
+            match key {
+                KeyCode::W => velocity += vert,
+                KeyCode::S => velocity -= vert,
+                KeyCode::A => velocity -= horiz,
+                KeyCode::D => velocity += horiz,
+                _ => (),
+            }
+        } 
+
+        velocity = velocity.normalize();
+
+        if !velocity.is_nan() {
+            transform.translation += velocity * time.delta_seconds() * 100.0
+        } 
+    } */
 }
